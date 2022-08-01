43e98115eb946b9a7298e39e8880d035bc2b2f00
Rehan Bari <rbari921@mtroyal.ca>
2022-08-01 16:51:58 -0600

    Final code

--- main.py
+++ main.py
@@ -13,3 +13,43 @@ import pandas
 #Playing with Pandas Library
 data = pandas.read_csv("weather_data.csv")
 print(data)
+
+#converting the dataframe into a dictionary
+data_dict = data.to_dict()
+print(data_dict)
+
+#converting series into python data
+temp_list = data["temp"].to_list()
+
+#calculating average temperature from csv file
+average_temp = sum(temp_list) / len(temp_list)
+print(f"Average temperature = {average_temp} ")
+
+#Using Pandas to do the calculation instead
+print(data["temp"].mean())
+
+#Getting max value of temparture data
+#https://pandas.pydata.org/docs/reference/series.html
+print(data["temp"].max())
+
+#You can get column data this way too by passing a Key, pandas converts columns into an attribute
+print(data.condition)
+
+#Getting Data in a particular row
+print(data[data.day == "Monday"])
+
+#Getting data from a row where temp = the maximum, which row had max temp?
+print(data[data.temp == data["temp"].max()])
+
+#You can tap into a rows specific attributes the same way you can with a dataframe in pandas
+monday = data[data.day == "Monday"]
+print(monday.condition)
+
+#Create a dataframe from scratch
+data_dict = {
+    "students": ["Amy", "James"],
+    "scores": [76, 120]
+}
+
+dict_as_df = pandas.DataFrame(data_dict)
+print(dict_as_df)
